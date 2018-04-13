436c6b03b49e5ba8c9c504a538c5b53c4187ac63
Nandhakumar <nandhakumaar.n@gmail.com>
2018-04-13 13:01:58 +0530

    Initial commit

---
+++ app/src/main/java/com/happysanz/hstin_house/utils/HSTInHouseConstants.java
@@ -0,0 +1,14 @@
+package com.happysanz.hstin_house.utils;
+
+public class HSTInHouseConstants {
+
+    // URLS
+    // BASE URL
+
+    public static final String BASE_URL = "http://happysanz.net/";
+
+    // PARAMETERS
+    //Service Params
+    public static String PARAM_MESSAGE = "msg";
+
+}
